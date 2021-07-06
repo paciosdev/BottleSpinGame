399bb9368c2baefb82816f43460cf3d52498055b
Francesco Paciello <kekkopacio@gmail.com>
2021-07-06 17:44:01 +0200

    Initial Commit

---
+++ bottle/bottleApp.swift
@@ -0,0 +1,17 @@
+//
+//  bottleApp.swift
+//  bottle
+//
+//  Created by Kekko Paciello on 06/07/21.
+//
+
+import SwiftUI
+
+@main
+struct bottleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
